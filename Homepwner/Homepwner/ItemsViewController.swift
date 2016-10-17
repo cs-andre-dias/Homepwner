1d6466dfc51ad1f81e2aceac774adf941187acfa
Andre Dias <andre.dias@concrete.com.br>
2016-10-17 16:16:44 -0200

    Adiciona inset para padding no topo das cells

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -17,7 +17,7 @@ class itemsViewController: UITableViewController{
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell" )
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
         
         let item = itemStore.allItems[indexPath.row]
         
@@ -29,5 +29,13 @@ class itemsViewController: UITableViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+        
     }
 }
